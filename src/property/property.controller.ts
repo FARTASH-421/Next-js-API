046b944cb2fada9072fd0aab9906774f4791b316
FARTASH-421 <abdul.qhadir.fartash@gmail.com>
2026-06-24 22:28:47 +0330

    impelement post api and dynamic route api for Get() and HttpCode() decartor

--- src/property/property.controller.ts
+++ src/property/property.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
 
 @Controller('property')
 export class PropertyController {
@@ -7,8 +7,14 @@ export class PropertyController {
     return 'hello all propery';
   }
 
+  @Get(':id/:slug')
+  findOne(@Param('id') id, @Param('slug') slug): any {
+    return `id = ${id} and slug = ${slug}`;
+  }
+
   @Post()
-  create() {
-    return 'this is post property api';
+  @HttpCode(202)
+  create(@Body('name') name): any {
+    return name;
   }
 }
